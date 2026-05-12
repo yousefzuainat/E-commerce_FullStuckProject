6e268d837d7beb45e548e1463cf586cce268a2b4
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-05-12 16:30:04 +0300

    add the migrition

---
+++ ecommerce system/Migrations/20260512112436_creattestimonials.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class creattestimonials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Testimonials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    IsApproved = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Testimonials", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Testimonials_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Testimonials_UserId",
+                table: "Testimonials",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Testimonials");
+        }
+    }
+}
---
+++ ecommerce system/Models/Testimonials.cs
@@ -0,0 +1,14 @@
+﻿namespace ecommerce_system.Models
+{
+    public class Testimonials
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public AppliactionUser User { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public bool IsApproved { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
