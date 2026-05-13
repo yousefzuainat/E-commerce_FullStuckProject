5a0cf85a6e8a0792cfc95aecd8f024605e0b7142
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-05-13 11:06:49 +0300

    add helper folder and file helper

---
+++ ecommerce system/Helper/FileHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace ecommerce_system.Helper
+{
+    public class FileHelper
+    {
+        public static async Task<string> UploadFileAsync(IFormFile file, string folderName)
+        {
+
+            if (file == null || file.Length == 0)
+            {
+                return null;
+            }
+
+            string uploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", folderName);
+
+            if (!Directory.Exists(uploadFolder))
+            {
+                Directory.CreateDirectory(uploadFolder);
+            }
+
+
+            string fileName = $"{Guid.NewGuid()}_{file.FileName}";
+            string filePath = Path.Combine(uploadFolder, fileName);
+
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return fileName;
+        }
+    }
+}
